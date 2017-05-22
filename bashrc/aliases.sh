1a071d4d8c5ebc10c236a82f80cd3dcfcc77e5b0
Mark Torres <mark.torres.mets@gmail.com>
2017-05-22 12:48:51 -0500

    Update bashrc files

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -5,7 +5,20 @@
 # Quick "cd" to specific directory, like your ~/Git folder if it exists
 if [ -d "$HOME/Git" ] ; then
 	alias gogit="cd $HOME/Git"
-else
-	alias gogit="echo Directory $HOME/Git does not exist."
 fi
 
+# GENERIC UNIX
+alias ls="ls -lhF"
+alias ping="ping -c 10 -i 2"
+alias go2up="cd ../.."
+alias go3up="cd ../../.."
+alias go4up="cd ../../../.."
+alias javafiles="find . -type f -name '*.java'"
+alias swiftfiles="find . -type f -name '*.swift'"
+alias phpfiles="find . -type f -name '*.php'"
+alias nmapssl="sudo nmap --script ssl-cert,ssl-enum-ciphers -p 443"
+alias dateinfo="date +'%a %b %d %Y, %k:%Mhrs - Week:%U, Day:%j'"
+
+# MAC OS
+alias adb="~/Library/Android/sdk/platform-tools/adb"
+alias cdsimdev="cd && cd Library/Developer/CoreSimulator/Devices/"
--- bashrc/tweaks.sh
+++ bashrc/tweaks.sh
@@ -1,5 +1,24 @@
+# nano for crontab edit
+export VISUAL=nano
 
 # PS1 is the promt string, this changes your promt to:
 # [user@hostname]<working-directory>$
 PS1="[\u@\h]<\W>\$ "
 
+# git promt {
+if [ -f ~/.git-prompt.sh ]; then
+	source ~/.git-prompt.sh
+	#export PS1='[\u@\h:\W$(__git_ps1 "(%s)")]\$ '
+	export PS1='\u\[$(tput sgr0)\]@\h\[$(tput sgr0)\][\[$(tput setaf 4)\]\W\[$(tput setaf 1)\]$(__git_ps1 "(%s)")\[$(tput sgr0)\]]$\[$(tput sgr0)\] '
+fi
+# }
+
+# load git autocompletion
+if [ -f ~/.git-completion.bash ]; then
+	source ~/.git-completion.bash
+fi
+
+# user bin directory
+if [[ -d $HOME/bin ]]; then
+	export PATH=$PATH:$HOME/bin
+fi
