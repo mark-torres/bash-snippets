f73a414a9943ac503efc9503aca544e43048e412
Mark Torres <mark.torres.mets@gmail.com>
2018-09-21 17:18:49 -0500

    Changes to iOS graphics scripts

--- misc/mkgraphic-ios.sh
+++ misc/mkgraphic-ios.sh
@@ -12,20 +12,25 @@ if [[ -z $3 ]]; then
 	exit 1
 fi
 
+# check size format
 if [[ ! $3 =~ [0-9]+x[0-9]+ ]]; then
-	echo The size must be in numeric WIDTHxHEIGHT format
+	echo The size must be in numeric WIDTHxHEIGHT format, e. g. 240x120
 	exit 1
 fi
 
-NAME=`echo $2 | sed -e 's/-/_/g'`
+NAME=$( echo $2 | sed -e 's/-/_/g' )
 
-FILE_EXT=`file --mime-type $1 | awk -Fimage/ '{print $2}'`
+FILE_EXT=$( file --mime-type $1 | awk -Fimage/ '{print $2}' )
 
-SIZE_1X=`echo $3|awk -Fx '{print int($1*1) "x" int($2*1)}'`
-SIZE_2X=`echo $3|awk -Fx '{print int($1*2) "x" int($2*2)}'`
-SIZE_3X=`echo $3|awk -Fx '{print int($1*3) "x" int($2*3)}'`
+WIDTH=$( echo $3 | awk -Fx '{print $1}' )
+HEIGHT=$( echo $3 | awk -Fx '{print $2}' )
 
-convert $1 -resize ${SIZE_1X} -colorspace sRGB $CWD/${NAME}_${3}pt.${FILE_EXT}
-convert $1 -resize ${SIZE_2X} -colorspace sRGB $CWD/${NAME}_${3}pt@2x.${FILE_EXT}
-convert $1 -resize ${SIZE_3X} -colorspace sRGB $CWD/${NAME}_${3}pt@3x.${FILE_EXT}
+let "WX2 = WIDTH * 2"
+let "HX2 = HEIGHT * 2"
 
+let "WX3 = WIDTH * 3"
+let "HX3 = HEIGHT * 3"
+
+convert $1 -resize ${WIDTH}x${HEIGHT} -colorspace sRGB $CWD/${NAME}_${3}pt.${FILE_EXT}
+convert $1 -resize ${WX2}x${HX2} -colorspace sRGB $CWD/${NAME}_${3}pt@2x.${FILE_EXT}
+convert $1 -resize ${WX3}x${HX3} -colorspace sRGB $CWD/${NAME}_${3}pt@3x.${FILE_EXT}
--- misc/mkicon-ios.sh
+++ misc/mkicon-ios.sh
@@ -2,22 +2,28 @@
 CWD=$(pwd)
 CONV_BIN=$(which convert)
 
-if [[ -z $CONV_BIN ]]; then
+if [ -z $CONV_BIN ]; then
 	echo ImageMagik convert is not installed
 	exit 1
 fi
 
-if [[ -z $3 ]]; then
+if [ -z $3 ]; then
 	echo Usage: $0 SRC_FILE DST_NAME BASE_SIZE
 	exit 1
 fi
 
-NAME=`echo $2 | sed -e 's/-/_/g'`
+# check size format
+if [[ ! $3 =~ [0-9]+ ]]; then
+	echo The size must be a number
+	exit 1
+fi
 
-SIZE_X1=$3
+NAME=`echo $2 | sed -e 's/-/_/g'`
+SIZE_X1=$( echo $3 | awk '{ print ($1 >= 0) ? $1 : 0 - $1}' )
+
 let "SIZE_X2 = SIZE_X1 * 2"
 let "SIZE_X3 = SIZE_X1 * 3"
 
-convert $1 -resize ${SIZE_X1}x${SIZE_X1} -colorspace sRGB $CWD/${NAME}_${3}pt.png
-convert $1 -resize ${SIZE_X2}x${SIZE_X2} -colorspace sRGB $CWD/${NAME}_${3}pt@2x.png
-convert $1 -resize ${SIZE_X3}x${SIZE_X3} -colorspace sRGB $CWD/${NAME}_${3}pt@3x.png
+echo convert $1 -resize ${SIZE_X1}x${SIZE_X1} -colorspace sRGB $CWD/${NAME}_${3}pt.png
+echo convert $1 -resize ${SIZE_X2}x${SIZE_X2} -colorspace sRGB $CWD/${NAME}_${3}pt@2x.png
+echo convert $1 -resize ${SIZE_X3}x${SIZE_X3} -colorspace sRGB $CWD/${NAME}_${3}pt@3x.png
