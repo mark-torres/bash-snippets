56dc4f427ce0e1aed7e689f7c226b30cfaf7ebc5
Mark Torres <mark.torres.mets@gmail.com>
2019-05-13 18:13:40 -0500

    Edit aliases

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -15,14 +15,18 @@ alias go3up="cd ../../.."
 alias go4up="cd ../../../.."
 alias javafiles="find . -type f -name '*.java'"
 alias swiftfiles="find . -type f -name '*.swift'"
+alias dartfiles="find . -type f -name '*.dart'"
 alias phpfiles="find . -type f -name '*.php'"
 alias nmapssl="sudo nmap --script ssl-cert,ssl-enum-ciphers -p 443"
 alias dateinfo="date +'%a %b %d %Y, %k:%Mhrs - Week:%U, Day:%j'"
 
 # MAC OS
 alias adb="~/Library/Android/sdk/platform-tools/adb"
-alias cdsimdev="cd && cd Library/Developer/CoreSimulator/Devices/"
 alias xcode-DerivedData="cd ~/Library/Developer/Xcode/DerivedData"
 alias libarch-info="lipo -info"
 test -f ~/Library/Android/sdk/platform-tools/adb && alias adb="~/Library/Android/sdk/platform-tools/adb"
 test -d ~/Library/Developer/CoreSimulator/Devices && alias cdsimdev="cd ~/Library/Developer/CoreSimulator/Devices/"
+
+# iOS Simulator app
+_SIMAPP=/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
+alias simulator="test -d $_SIMAPP && open $_SIMAPP; test -d $_SIMAPP || echo Simulator not found"
